201d5ef20f393a69885f4cfc3155bbfd037cafac
Red Markham <redmark@netzero.net>
2018-03-11 20:21:57 -0400

    Added the put request

--- routes/api.js
+++ routes/api.js
@@ -4,27 +4,29 @@ const constants = require('../constants');
 const ninjaModel = require('../models/ninja');
 
 router.get(constants.ningaRoute, (req, res, next) => {
-   res.send({
-      type: 'GET'
-   });
+  res.send({
+    type: 'GET'
+  });
 });
 
 router.post(constants.ningaRoute, (req, res, next) => {
-  // Add ninja to the db
-  ninjaModel.create(req.body).then(function(ninja) {
+  ninjaModel.create(req.body).then(function (ninja) {
     res.send(ninja);
   }).catch(next);
 });
 
+
 router.put(constants.ninjaRouteId, (req, res, next) => {
-   res.send({
-      type: 'PUT',
-      body: req.body
-   });
+  ninjaModel.findByIdAndUpdate({_id: req.params.id}, req.body).then(function () {
+    ninjaModel.findOne({_id: req.params.id}).then(function (ninja) {
+      res.send(ninja);
+    });
+  }).catch(next);
 });
 
+
 router.delete(constants.ninjaRouteId, (req, res, next) => {
-  ninjaModel.findByIdAndRemove({_id: req.params.id}).then(function(ninja) {
+  ninjaModel.findByIdAndRemove({_id: req.params.id}).then(function (ninja) {
     res.send(ninja);
   }).catch(next);
 });
